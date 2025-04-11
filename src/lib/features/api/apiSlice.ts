9b0f3801c21bbf8b58a20e34385e17e9ec0dfdcb
ahmmed-shakil <shakilahmmed252@gmail.com>
2025-04-12 00:24:25 +0600

    Sturtcure update

--- src/lib/features/api/apiSlice.ts
+++ src/lib/features/api/apiSlice.ts
@@ -1,25 +1,26 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { RootState } from '../store';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { RootState } from "../store";
 
 // Define base query with authorization header
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000/api',
+  baseUrl: "https://chat-application-agpb.onrender.com/api",
+  // baseUrl: 'http://localhost:5000/api',
   prepareHeaders: (headers, { getState }) => {
     // Add token to request headers
     const token = (getState() as RootState).auth.token;
     if (token) {
-      headers.set('authorization', `Bearer ${token}`);
+      headers.set("authorization", `Bearer ${token}`);
     }
     return headers;
   },
-  credentials: 'include',
+  credentials: "include",
 });
 
 // Create API slice
 export const apiSlice = createApi({
-  reducerPath: 'api',
+  reducerPath: "api",
   baseQuery,
-  tagTypes: ['User', 'Chat', 'Message'],
+  tagTypes: ["User", "Chat", "Message"],
   endpoints: () => ({}),
 });
 
